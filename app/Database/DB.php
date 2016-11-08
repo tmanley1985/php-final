ac4fd4087d2b45c7bf9803706f840842aaa9d2e2
vagrant <vagrant@homestead>
2016-11-08 18:39:23 +0000

    Added basic structure for controller and model as well as a connection class

---
+++ app/Database/DB.php
@@ -0,0 +1,5 @@
+<?php namespace App\Database;
+
+class DB {
+	
+}
--- app/Models/Model.php
+++ app/Models/Model.php
@@ -1,5 +1,43 @@
-<?php
+<?php namespace App\Models;
+
+use App\Database\DB;
 
 class Model {
-	
+
+	protected $connection;
+
+	public function __construct(DB $connection)
+	{
+		$this->connection = $connection;
+	}
+
+	public static function find($id)
+	{
+
+	}
+
+	public static function findAll()
+	{
+
+	}
+
+	public function save()
+	{
+
+	}
+
+	public function destroy()
+	{
+
+	}
+
+	public function update()
+	{
+
+	}
+
+	public function get()
+	{
+
+	}
 }
