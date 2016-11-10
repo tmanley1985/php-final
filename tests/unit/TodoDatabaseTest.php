6a9d085847fba43e47b6ffeb44e8a4c5e137b943
tmanley1985 <tmanley1985@gmail.com>
2016-11-10 16:51:02 +0000

    Added xml seed file and implementing two abstract methods

--- tests/unit/TodoDatabaseTest.php
+++ tests/unit/TodoDatabaseTest.php
@@ -5,5 +5,42 @@ use \Faker\Factory as Faker;
 
 class TodoDatabaseTest extends \PHPUnit_Extensions_Database_TestCase
 {
-	
+	/**
+    * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+    */
+    public function getConnection()
+    {
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=homestead', 'homestead', 'secret');
+        
+        return $this->createDefaultDBConnection($pdo,'homestead');
+    }
+
+    /**
+    * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+    */
+    public function getDataSet()
+    {
+        return $this->createMySQLXMLDataSet(__DIR__ . '/seed.xml');
+
+    }
+
+    /** @test */
+
+	public function canGetConnection()
+	{
+		$this->getConnection()->createDataSet(array('todos'));
+
+        $todo = $this->getDataSet();
+
+        $queryTable = $this->getConnection()->createQueryTable(
+
+            'todos', 'SELECT * FROM todos'
+        );
+
+        $expectedTable = $this->getDataSet()->getTable('todos');
+
+        //Here we check that the table in the database matches the data in the XML file
+        $this->assertTablesEqual($expectedTable, $queryTable);
+	}
+
 }
